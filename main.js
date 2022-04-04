ff4b05bb4769750ce6f57c015d0d8a731ee75b68
JunyiShi <junyi.shi@yale.edu>
2022-04-03 21:36:09 -0400

    update

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
-// this is where you write your javascript
-
-$(window).keydown(function(e){
-
-  var key = e.key;
-
-  console.log(key);
-
-  if (key == "a") {
-    $(".message").html("Apple");
+$("#nameCheck").change(function () {
+  $(".name").hide();
+  if ($("#nameCheck").is(":checked")) {
+    $(".name").show();
   }
-  else if (key == "b") {
-    $(".message").html("Bagpipes");
-  }
-  else if (key == "c") {
-    $(".message").html("Cadillac");
-  } 
-
 });
 
+$("#otherCheck").change(function () {
+  $(".otherName").hide();
+  if ($("#otherCheck").is(":checked")) {
+    $(".otherName").show();
+  }
+});
 
+$("#originalCheck").change(function () {
+  $(".original").hide();
+  if ($("#originalCheck").is(":checked")) {
+    $(".original").show();
+  }
+});
 
+$("#whereCheck").change(function () {
+  $(".where").hide();
+  if ($("#whereCheck").is(":checked")) {
+    $(".where").show();
+  }
+});
+
+$("#spritualCheck").change(function () {
+  $(".spiritual").hide();
+  if ($("#spritualCheck").is(":checked")) {
+    $(".spiritual").show();
+  }
+});
+
+$("#storyCheck").change(function () {
+  $(".story").hide();
+  if ($("#storyCheck").is(":checked")) {
+    $(".story").show();
+  }
+});
+
+$("#plantCheck").change(function () {
+  $(".plantImage").hide();
+  $(".plant").hide();
+  if ($("#plantCheck").is(":checked")) {
+    $(".plantImage").show();
+    $(".plant").show();
+  }
+});
